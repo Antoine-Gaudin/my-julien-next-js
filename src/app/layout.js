0db8e25ac494d6f25071710d82f593a05c647add
gaudin <agaudin76@gmail.com>
2025-09-07 01:09:17 +0200

    ajout google analytique

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import NameTag from "./componants/NameTag";
 import Navigation from "./componants/Navigation";
 import Footer from "./componants/Footer";
+import Script from "next/script"; // 👈 ajoute ça
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,14 +17,14 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Vintage Automobiles 1856",
-  description: "Vintage automobiles 1856 est spécialisé dans la vente et la reprise de véhicules récents multimarques mais propose et reprend également des automobiles de collection - youngtimer. Nous sommes à votre disposition, n’hésitez pas à prendre contact pour tous conseils !",
-
+  description:
+    "Vintage automobiles 1856 est spécialisé dans la vente et la reprise de véhicules récents multimarques mais propose et reprend également des automobiles de collection - youngtimer. Nous sommes à votre disposition, n’hésitez pas à prendre contact pour tous conseils !",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-            <head>
+    <html lang="fr">
+      <head>
         {/* Ajout direct de Font Awesome */}
         <link
           rel="stylesheet"
@@ -33,10 +34,24 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NameTag/>
-        <Navigation/>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HBSHMTFK39"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HBSHMTFK39');
+          `}
+        </Script>
+
+        <NameTag />
+        <Navigation />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
