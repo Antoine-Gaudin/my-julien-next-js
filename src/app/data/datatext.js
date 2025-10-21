ba9d9d3bdd86266c60bfb871f0fc14ae2d260f29
gaudin <agaudin76@gmail.com>
2025-10-21 23:44:07 +0200

    Modification Orthographe

--- src/app/data/datatext.js
+++ src/app/data/datatext.js
@@ -1,7 +1,7 @@
 const DataText = [
   {
     id: 1,
-    title: "Vintage automobile 1856 à Saint Jacque sur darnetale",
+    title: "Vintage automobile 1856 à Saint Jacque sur darnetal",
     text: "Vintage automobiles 1856 est spécialisé dans la vente et la reprise de véhicules récents multimarques mais propose et reprend également des automobiles de collection - Nous sommes à votre disposition, n’hésitez pas à prendre contact pour tous conseils !",
   }
 ];
